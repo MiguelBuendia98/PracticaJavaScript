9b45c0134823aa68fe7f832e8d66d47104477d84
MiguelBuendia98 <miguelbuendiaparra@hotmail.com>
2022-05-31 14:15:21 -0500

    fin del curso

---
+++ salarios.js
@@ -0,0 +1,57 @@
+const colombia = [];
+
+colombia.push
+(
+    {
+        name: "Miguel",
+        salary: 1000,
+    }
+);
+
+colombia.push
+(
+    {
+        name: "Felipe",
+        salary: 500,
+    }
+);
+
+colombia.push
+(
+    {
+        name: "Snayder",
+        salary: 700,
+    }
+);
+
+colombia.push
+(
+    {
+        name: "Tatiana",
+        salary: 1500,
+    }
+);
+
+colombia.push
+(
+    {
+        name: "Natalia",
+        salary: 2500,
+    }
+);
+
+colombia.push
+(
+    {
+        name: "Juan Jose",
+        salary: 300,
+    }
+);
+
+colombia.push
+(
+    {
+        name: "Kevin",
+        salary: 1300,
+    }
+);
